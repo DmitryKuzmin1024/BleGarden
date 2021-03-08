8cfdd34f897a6aaf4dfc1ff3b9b911eff9018cf1
kuzya <infinitusconscientia@gmail.com>
2021-03-09 01:14:56 +0300

    Enum for values

---
+++ src/main/java/com/example/blegarden/BytesValue.kt
@@ -0,0 +1,14 @@
+package com.example.blegarden
+
+enum class BytesValue(val value: Byte) {
+    LED_ON(1),
+    LED_OFF(2),
+    FUN_ON(3),
+    FUN_OFF(4),
+    CMP_ON(5),
+    CMP_OFF(6),
+    MODE_ZERO(7),
+    MODE_ONE(8),
+    MODE_TWO(9),
+    KILL_ALL(0)
+}
